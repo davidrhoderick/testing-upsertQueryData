3dd8b5fdc2485a5a103291b41f2d1bb5d6dcc59c
David Rhoderick <broken.stairs@gmail.com>
2022-08-30 13:10:24 -0400

    Cleaning up _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,13 @@
 import * as React from 'react';
 
-import Head from 'next/head';
-
 import { store } from '../redux/store';
 import { Provider as ReduxProvider } from 'react-redux';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Secure Checkout</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-
-      <ReduxProvider store={store}>
-        <Component {...pageProps} />
-      </ReduxProvider>
-    </>
+    <ReduxProvider store={store}>
+      <Component {...pageProps} />
+    </ReduxProvider>
   );
 }
 
